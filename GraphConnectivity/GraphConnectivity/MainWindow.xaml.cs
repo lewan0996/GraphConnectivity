eef026aebe3e2390a4570da237b400a09e16ce4a
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-04-27 23:47:00 +0200

    Solution files+quick test

---
+++ GraphConnectivity/GraphConnectivity/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GraphConnectivity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            // GraphGeneration can be injected via the IGraphGeneration interface
+
+            var wrapper = new GraphGeneration(getStartProcessQuery,
+                                              getProcessStartInfoQuery,
+                                              registerLayoutPluginCommand);
+
+            byte[] output = wrapper.GenerateGraph("digraph{Rafał -> Motyka; Motyka -> RaV; Motyka -> Motyka96; Motyka96->Rafał;Motyka->Rafał;}", Enums.GraphReturnType.Png);
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(output);
+            bitmapImage.EndInit();
+
+            imgGraph.Source = bitmapImage;
+        }
+    }
+}
