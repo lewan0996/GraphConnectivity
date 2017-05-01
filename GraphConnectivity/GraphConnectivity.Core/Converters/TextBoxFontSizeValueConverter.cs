3304e90261c855b99a67302e00656472c67b1409
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-05-01 20:54:28 +0200

    TextBoxValueConverter

---
+++ GraphConnectivity/GraphConnectivity.Core/Converters/TextBoxFontSizeValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
+
+namespace GraphConnectivity.Core.Converters
+{
+    public class TextBoxFontSizeValueConverter:MvxValueConverter<double, double>
+    {
+        protected override double Convert(double value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value / 1.5;
+        }
+
+        protected override double ConvertBack(double value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value * 1.5;
+        }
+    }
+}
