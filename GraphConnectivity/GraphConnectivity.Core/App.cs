bb575e1af8e3d929fc05a2e99ca3f6968b9caa0b
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2017-05-01 22:07:18 +0200

    Porządek w referencjach

--- GraphConnectivity/GraphConnectivity.Core/App.cs
+++ GraphConnectivity/GraphConnectivity.Core/App.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using GraphConnectivity.Core.ViewModels;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform;
--- GraphConnectivity/GraphConnectivity.Core/Services/IGraphVizualiser.cs
+++ GraphConnectivity/GraphConnectivity.Core/Services/IGraphVizualiser.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using GraphConnectivity.Core.Models;
 
 namespace GraphConnectivity.Core.Services
